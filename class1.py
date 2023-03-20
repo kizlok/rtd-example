f1106b1dbe98cfe330c1ff5d88d4e92cf665c8ac
Grzegorz Kamiński <grzegorz.kaminski@apagroup.pl>
2023-03-20 16:07:56 +0100

    2 pliki

---
+++ class1.py
@@ -0,0 +1,14 @@
+from class2 import Class2
+
+
+class Class1:
+    """Class1 opis
+    """
+
+    def __init__(self):
+        """ class1 init
+
+        """
+
+        """atrybut opis"""
+        self.a2 = Class2()
---
+++ class2.py
@@ -0,0 +1,9 @@
+class Class2:
+    """Class2 opis"""
+    def __init__(self):
+        """ opis init
+
+        """
+
+        """atrybut clasy2 """
+        self.a1 = "Atrybut"
